17a281a3866e5de486f29847304d440f38730807
Robert <robert.j.rantanen@helsinki.fi>
2019-11-02 22:32:28 +0200

    ensimmäisiä testejä

---
+++ ristinolla/src/test/java/algoritmit/minimaxTest.java
@@ -0,0 +1,49 @@
+package algoritmit;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class minimaxTest {
+
+    private String[][] lauta;
+    private minimax minimax;
+
+    public minimaxTest() {
+        lauta = new String[][]{
+            {"X", "O", "X"},
+            {"O", "O", "X"},
+            {"_", "_", "_"}};
+        minimax = new minimax();
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void parasLiikeXMerkilla() {
+        assertEquals("2 2", minimax.parasLiike(lauta, "X"));
+    }
+
+    @Test
+    public void parasLiikeOMerkilla() {
+        assertEquals("2 1", minimax.parasLiike(lauta, "O"));
+    }
+
+}
